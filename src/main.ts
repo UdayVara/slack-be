626130bc11fbcb8795d1153ef90fef2adbd39237
Uday vara <varaudayd@gmail.com>
2025-09-04 00:17:55 +0530

    added global api prefix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix("api/v1");
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
